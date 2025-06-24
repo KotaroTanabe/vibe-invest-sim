99e6cc6c527efc10f6df289363e30e667927c186
KotaroTanabe <tanabek634@gmail.com>
2025-06-24 13:47:49 +0900

    feat: allow custom monthly investment

--- test/test.js
+++ test/test.js
@@ -10,4 +10,7 @@ data = generateMockData('2024-01', '2024-03');
 assert.strictEqual(data.labels.length, 3, 'range should reflect months count');
 assert.strictEqual(data.labels[0], '2024-01');
 assert.strictEqual(data.labels[2], '2024-03');
+
+data = generateMockData('2024-01', '2024-01', 50000);
+assert.strictEqual(data.investmentData[0], 50000, 'monthly amount should be applied');
 console.log('All tests passed');
